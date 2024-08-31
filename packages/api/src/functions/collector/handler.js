0c228a9f98303832b16943d39bbf4823c0120d67
Thomas Schoffelen <thomas@schof.co>
2024-08-31 13:49:15 +0100

    feat: init frontend

--- packages/api/src/functions/collector/handler.js
+++ packages/api/src/functions/collector/handler.js
@@ -7,46 +7,58 @@ export const app = async ({ body }) => {
 
     if (span.type === "log") {
       // save log span
-      await put({
-        ...span,
-        pk: `transaction#${span.transactionId || span.transaction_id}`,
-        sk: `log#${span.started}#${span.id}`,
-        type: "log",
-      });
+      await put(
+        {
+          ...span,
+          pk: `transaction#${span.transactionId || span.transaction_id}`,
+          sk: `log#${span.started}#${span.id}`,
+          type: "log",
+        },
+        true,
+      );
       continue;
     }
 
     // save transaction span
-    await put({
-      ...span,
-      pk: `transaction#${span.transactionId || span.transaction_id}`,
-      sk: `span#${span.started || span.sending_time}#${span.id}`,
-      type: "span",
-      spanType: span.type,
-    });
+    await put(
+      {
+        ...span,
+        pk: `transaction#${span.transactionId || span.transaction_id}`,
+        sk: `span#${span.started || span.sending_time}#${span.id}`,
+        type: "span",
+        spanType: span.type,
+      },
+      true,
+    );
 
     if (span.type === "function" && span.ended) {
       // save function invocation details
-      await put({
-        ...span,
-        pk: `function#${span.name}`,
-        sk: `invocation#${span.started}#${span.id}`,
-        type: "invocation",
-      });
+      await put(
+        {
+          ...span,
+          pk: `function#${span.name}`,
+          sk: `invocation#${span.started}#${span.id}`,
+          type: "invocation",
+        },
+        true,
+      );
 
       // save function meta data
-      await put({
-        pk: `function#${span.name}`,
-        sk: `function`,
-        type: "function",
-        lastInvocation: span.started,
-        runtime: span.runtime,
-        account: span.account,
-        region: span.region,
-        arn: span.invokedArn,
-        memoryAllocation: span.memoryAllocation,
-        timeout: span.maxFinishTime - span.started,
-      });
+      await put(
+        {
+          pk: `function#${span.name}`,
+          sk: `function`,
+          type: "function",
+          lastInvocation: span.started,
+          runtime: span.runtime,
+          account: span.account,
+          region: span.region,
+          arn: span.invokedArn,
+          memoryAllocation: span.memoryAllocation,
+          timeout: span.maxFinishTime - span.started,
+        },
+        true,
+      );
     }
   }
 };
