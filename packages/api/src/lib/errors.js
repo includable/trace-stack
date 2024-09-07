0dfcac33d8f769075b3fcedf4d4e0d90983cec1e
Thomas Schoffelen <thomas@schof.co>
2024-09-07 16:17:03 +0100

    feat: remove unix timestamps

--- packages/api/src/lib/errors.js
+++ packages/api/src/lib/errors.js
@@ -1,7 +1,14 @@
 import slug from "slug";
 
 const removeUniqueIds = (str) => {
-  return str.replace(/[a-f0-9]([a-f0-9-]{24,34})[a-f0-9]/g, "{id}");
+  // uuids
+  str = str.replace(/[a-f0-9]([a-f0-9-]{24,34})[a-f0-9]/g, "{id}");
+
+  // unix and epoch timestamps
+  str = str.replace(/[0-9]{13}/g, "{timestamp}");
+  str = str.replace(/[0-9]{10}/g, "{timestamp}");
+
+  return str;
 };
 
 export const getErrorKey = (error) => {
