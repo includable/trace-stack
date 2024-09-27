bd3629daa1dc98a4368ac87eb09ee7082b73682b
Thomas Schoffelen <thomas@schof.co>
2024-09-27 18:46:30 +0100

    chore: cleanup

--- packages/e2e-test/tests/errors.test.js
+++ packages/e2e-test/tests/errors.test.js
@@ -10,13 +10,4 @@ describe("Errors", () => {
     expect(errors).toHaveLength(1);
     expect(errors[0].error.type).toBe("Error");
   }, 15_000);
-
-  //   it("should capture memory errors", async () => {
-  //     await invoke("error-memory");
-
-  //     const errors = await getErrors();
-  //     console.log(errors[0]);
-
-  //     expect(errors).toHaveLength(1);
-  //   }, 15_000);
 });
