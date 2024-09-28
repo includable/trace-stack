83b16f536b92142ccbaa7ac083e52eb4ea9118e7
Thomas Schoffelen <thomas@schof.co>
2024-09-28 14:52:42 +0100

    fix: improve status code reporting

--- packages/api/src/lib/invocations.js
+++ packages/api/src/lib/invocations.js
@@ -2,12 +2,19 @@ import { put } from "./database";
 
 const getStatusCodeFromSpan = (span) => {
   if (!span.return_value) return null;
+
+  // TODO: support Lambda Function URLs
+  if (!span.info?.trigger?.some((trigger) => trigger?.triggeredBy === "apigw"))
+    return null;
+
   try {
     if (typeof span.return_value === "object") {
       return span.return_value?.statusCode?.toString() || null;
     }
     if (typeof span.return_value === "string") {
-      const returnValue = JSON.parse(span.return_value);
+      const returnValue = JSON.parse(
+        span.return_value.replace(/\.\.\.\[too long\]$/, ""),
+      );
       return returnValue?.statusCode?.toString() || null;
     }
   } catch (e) {}
